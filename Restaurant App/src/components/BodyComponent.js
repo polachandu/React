dc0fb923f0692c92c4ee2bc4962ee3fff9eae3e3
polachandu <polachandrahas@gmail.com>
2023-10-01 21:00:04 -0700

    Top Rated Restaurants

--- Restaurant App/src/components/BodyComponent.js
+++ Restaurant App/src/components/BodyComponent.js
@@ -1,19 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
 import carts from "../utils/mockData";
-
+import { useState } from "react";
 const BodyComponent = () => {
+    const [listOfCarts, setCarts]= useState(carts);
     return (
       <div className="body">
         <div className="filter">
-            <button className="filter-btn" onClick={()=>{cart = carts.filter(restaurant => restaurant.card.card.info.avgRating >= 4.0 ); <RestaurantCard resData={cart}/>}}>Top Rated Restaurants</button>
+            <button className="filter-btn" onClick={() => {
+                const filterCarts = listOfCarts.filter((cart) => cart.card.card.info.avgRating>4.0)
+                setCarts(filterCarts);
+            }}>Top Rated Restaurants</button>
         </div>
         <div className="RestroContainer">
-          {carts.map((restaurant) => (
+          {listOfCarts.map((restaurant) => (
             <RestaurantCard key={restaurant.card.card.info.id} resData={restaurant} />
           ))}
         </div>
       </div>
-    );
-  };
+);
+};
+  
 
   export default BodyComponent;
