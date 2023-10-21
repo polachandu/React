e672b1bfc4eef3f83655e5e77ee27d7ed348f7e9
polachandu <polachandrahas@gmail.com>
2023-10-21 12:28:12 -0700

    Intro to Class Components

--- Restaurant App/src/components/AboutUs.js
+++ Restaurant App/src/components/AboutUs.js
@@ -1,9 +1,11 @@
+import User from "./User";
 const AboutUs = () => {
     return (
         <div>
             <h1>About Us</h1>
+            <User />
         </div>
-    )
+    );
 };
 
 export default AboutUs;
---
+++ Restaurant App/src/components/User.js
@@ -0,0 +1,9 @@
+const User = () => {
+    return <div className="user-card">
+        <h1>Chandrahas Reddy</h1>
+        <h2>Vancouver</h2>
+        <h3>Chef</h3>
+    </div>
+};
+
+export default User;
